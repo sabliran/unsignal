909c19aa900f1d57ab02626dd05399bd5673ae6e
unknown <troungchanh@gmail.com>
2020-11-29 10:28:59 +0200

    change resolution

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -2,9 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {   
     public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropDown;
+    void Start() 
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -15,4 +48,8 @@ public class SettingsMenu : MonoBehaviour
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
 }
