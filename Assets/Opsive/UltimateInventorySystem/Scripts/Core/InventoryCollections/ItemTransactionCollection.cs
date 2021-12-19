112d370fcccda4ea47d08a42d27d17be5268330a
Savvas Fam <troungchanh@gmail.com>
2021-12-20 01:51:52 +0200

    inventory installed
    error with input system

---
+++ Assets/Opsive/UltimateInventorySystem/Scripts/Core/InventoryCollections/ItemTransactionCollection.cs
@@ -0,0 +1,145 @@
+﻿/// ---------------------------------------------
+/// Ultimate Inventory System
+/// Copyright (c) Opsive. All Rights Reserved.
+/// https://www.opsive.com
+/// ---------------------------------------------
+
+namespace Opsive.UltimateInventorySystem.Core.InventoryCollections
+{
+    using Opsive.UltimateInventorySystem.Core.DataStructures;
+    using Opsive.UltimateInventorySystem.UI.DataContainers;
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using EventHandler = Opsive.Shared.Events.EventHandler;
+
+    /// <summary>
+    /// An ItemCollection that sends items to other ItemCollections by checking where it fits.
+    /// </summary>
+    [Serializable]
+    public class ItemTransactionCollection : ItemCollection
+    {
+        [Tooltip(
+            "The item collection where the item will be transferred to, Make sure to add restrictions to does collections.")]
+        [SerializeField] protected List<string> m_ItemCollectionNames;
+        [Tooltip(
+            "Should an item that gets rejected The action that should be performed if the itemCollection is about to overflow.")]
+        [SerializeField] protected bool m_OverflowBackToOrigin;
+        [Tooltip("The actions performed on an item that was rejected from being added.")]
+        [SerializeField] protected CategoryItemActionSet m_RejectedItemActions;
+        [Tooltip(
+            "Return the item amount that was actually added in the right item collection or return also the amount that was rejected (in case it was added externally)?")]
+        [SerializeField] protected bool m_ReturnRealAddedItemAmount;
+
+        protected ItemCollection m_AddingToCollection;
+
+        /// <summary>
+        /// Check if the item can be added to this item collection.
+        /// </summary>
+        /// <param name="itemInfo">The item info to add.</param>
+        /// <returns>The item info to add.</returns>
+        public override ItemInfo? CanAddItem(ItemInfo itemInfo)
+        {
+            for (int i = 0; i < m_ItemCollectionNames.Count; i++) {
+                var itemCollection = m_Inventory.GetItemCollection(m_ItemCollectionNames[i]);
+                var result = itemCollection.CanAddItem(itemInfo);
+                if (result != null && result.Value.Amount != 0) {
+                    m_AddingToCollection = itemCollection;
+                    return itemInfo;
+                }
+            }
+
+            m_AddingToCollection = null;
+            return itemInfo;
+        }
+
+        /// <summary>
+        /// Add the item to the item collection where it fits.
+        /// </summary>
+        /// <param name="itemInfo">The item info to add.</param>
+        /// <param name="stackTarget">The item stack where it should be added.</param>
+        /// <param name="notifyAdd">Notify that the item was added?</param>
+        /// <returns></returns>
+        protected override ItemInfo AddInternal(ItemInfo itemInfo, ItemStack stackTarget, bool notifyAdd = true)
+        {
+            if (m_AddingToCollection == null) {
+                RejectItem(itemInfo, itemInfo);
+
+                if (m_ReturnRealAddedItemAmount) { return (ItemInfo) (0, itemInfo); }
+
+                return itemInfo;
+            }
+
+            var itemInfoToAdd = new ItemInfo(itemInfo.ItemAmount);
+            var addedItemInfo = m_AddingToCollection.AddItem(itemInfoToAdd);
+
+            if (addedItemInfo.Amount < itemInfo.Amount) {
+                var amountNotAdded = itemInfo.Amount - addedItemInfo.Amount;
+                var itemInfoRejected = (ItemInfo) (amountNotAdded, itemInfo);
+
+                RejectItem(itemInfo, itemInfoRejected);
+            }
+
+            if (m_ReturnRealAddedItemAmount) { return addedItemInfo; } else {
+                return (ItemInfo) (itemInfo.Amount, addedItemInfo);
+            }
+        }
+
+        /// <summary>
+        /// Reject the item when it cannot be added to the item collection.
+        /// </summary>
+        /// <param name="itemInfo">The original item info that was tried to be added.</param>
+        /// <param name="itemInfoRejected">The item info, with the amount that was rejected</param>
+        protected virtual void RejectItem(ItemInfo itemInfo, ItemInfo itemInfoRejected)
+        {
+            if (m_OverflowBackToOrigin && itemInfo.ItemCollection != null && itemInfo.ItemCollection != this) {
+                var returnedItemInfo = itemInfo.ItemCollection.AddItem(itemInfoRejected);
+            } else {
+                m_RejectedItemActions?.UseAllItemActions(itemInfoRejected, m_Inventory.ItemUser);
+            }
+
+            EventHandler.ExecuteEvent<ItemInfo>(m_Inventory, EventNames.c_Inventory_OnRejected_ItemInfo,
+                itemInfoRejected);
+        }
+
+        /// <summary>
+        /// Remove the item.
+        /// </summary>
+        /// <param name="itemInfo">THe item to remove.</param>
+        /// <returns>The item removed.</returns>
+        protected override ItemInfo RemoveInternal(ItemInfo itemInfo)
+        {
+            var amountToRemove = itemInfo.Amount;
+            var AmountRemoved = 0;
+            ItemInfo itemInfoRemoved = new ItemInfo(itemInfo.ItemAmount);
+            for (int i = 0; i < m_ItemCollectionNames.Count; i++) {
+                var itemCollection = m_Inventory.GetItemCollection(m_ItemCollectionNames[i]);
+                itemInfoRemoved = itemCollection.RemoveItem((amountToRemove, itemInfo));
+                AmountRemoved += itemInfoRemoved.Amount;
+                amountToRemove -= itemInfoRemoved.Amount;
+
+                if (amountToRemove <= 0) { return (itemInfo.Amount, itemInfoRemoved); }
+            }
+
+            return (AmountRemoved, itemInfoRemoved);
+        }
+
+        /// <summary>
+        /// Get the item info in the linked item collections.
+        /// </summary>
+        /// <param name="item">The item to get.</param>
+        /// <returns>The item info.</returns>
+        public override ItemInfo? GetItemInfo(Item item)
+        {
+            for (int i = 0; i < m_ItemCollectionNames.Count; i++) {
+                var itemCollection = m_Inventory.GetItemCollection(m_ItemCollectionNames[i]);
+
+                var itemInfo = itemCollection.GetItemInfo(item);
+
+                if (itemInfo.HasValue) { return itemInfo; }
+            }
+
+            return null;
+        }
+    }
+}
--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -105,6 +105,14 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Open Panel"",
+                    ""type"": ""Button"",
+                    ""id"": ""3ffd22c0-122a-46f3-ae8d-7c8ef786a080"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -371,6 +379,28 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""action"": ""ActivateButton"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""fd81440f-d4c2-4fdf-8d7b-17bc2778461d"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Open Panel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0b29b37b-5dc4-469f-9863-d29f52e807ab"",
+                    ""path"": ""<Keyboard>/i"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Open Panel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         },
@@ -461,6 +491,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         m_Player_RotateGun = m_Player.FindAction("RotateGun", throwIfNotFound: true);
         m_Player_MeleeAttack = m_Player.FindAction("MeleeAttack", throwIfNotFound: true);
         m_Player_ActivateButton = m_Player.FindAction("ActivateButton", throwIfNotFound: true);
+        m_Player_OpenPanel = m_Player.FindAction("Open Panel", throwIfNotFound: true);
         // Player_Map
         m_Player_Map = asset.FindActionMap("Player_Map", throwIfNotFound: true);
         m_Player_Map_Movement = m_Player_Map.FindAction("Movement", throwIfNotFound: true);
@@ -524,6 +555,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
     private readonly InputAction m_Player_RotateGun;
     private readonly InputAction m_Player_MeleeAttack;
     private readonly InputAction m_Player_ActivateButton;
+    private readonly InputAction m_Player_OpenPanel;
     public struct PlayerActions
     {
         private @PlayerControls m_Wrapper;
@@ -539,6 +571,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         public InputAction @RotateGun => m_Wrapper.m_Player_RotateGun;
         public InputAction @MeleeAttack => m_Wrapper.m_Player_MeleeAttack;
         public InputAction @ActivateButton => m_Wrapper.m_Player_ActivateButton;
+        public InputAction @OpenPanel => m_Wrapper.m_Player_OpenPanel;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -581,6 +614,9 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                 @ActivateButton.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnActivateButton;
                 @ActivateButton.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnActivateButton;
                 @ActivateButton.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnActivateButton;
+                @OpenPanel.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnOpenPanel;
+                @OpenPanel.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnOpenPanel;
+                @OpenPanel.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnOpenPanel;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -618,6 +654,9 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                 @ActivateButton.started += instance.OnActivateButton;
                 @ActivateButton.performed += instance.OnActivateButton;
                 @ActivateButton.canceled += instance.OnActivateButton;
+                @OpenPanel.started += instance.OnOpenPanel;
+                @OpenPanel.performed += instance.OnOpenPanel;
+                @OpenPanel.canceled += instance.OnOpenPanel;
             }
         }
     }
@@ -668,6 +707,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         void OnRotateGun(InputAction.CallbackContext context);
         void OnMeleeAttack(InputAction.CallbackContext context);
         void OnActivateButton(InputAction.CallbackContext context);
+        void OnOpenPanel(InputAction.CallbackContext context);
     }
     public interface IPlayer_MapActions
     {
